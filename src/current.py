3e7d7b0d015aefe56a782221285a4a1b116408c3
Cactochan <85914469+merwin-asm@users.noreply.github.com>
2024-07-14 23:31:56 +0530

    Update current.py

--- src/current.py
+++ src/current.py
@@ -1,4 +1,14 @@
 import time
+import os
+
+try:
+  f = open("/etc/os-settings.json")
+except:
+  os.system("sudo pacman -S archinstall")
+  os.system("archinstall")
+  f = open("/etc/os-settings.json", "w")
+  f.write({})
+  f.close()
 
 while True:
   print("HELLO")
